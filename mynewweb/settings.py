e495adb9b998a87b4daf7c26a8d3c48262f17e21
ianshuraj <rajanshu06121997@gmail.com>
2020-06-21 21:17:46 +0530

    second

--- mynewweb/settings.py
+++ mynewweb/settings.py
@@ -125,3 +125,7 @@ os.path.join(BASE_DIR,'mynewweb/static')
 
 STATIC_ROOT=os.path.join(BASE_DIR,'static')
 STATIC_URL = '/static/'
+
+
+MEDIA_ROOT=os.path.join(BASE_DIR,'media')
+MEDIA_URL = '/media/'
